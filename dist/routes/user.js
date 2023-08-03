11036b678e75e75091523ad059a759dcc5ed9ddc
heryanto <71817100+heryanto87@users.noreply.github.com>
2023-08-03 21:42:32 +0700

    asdasdasd

--- dist/routes/user.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { userLogin, userRegister } from '../controllers/user.js';
-const router = express.Router();
-router.get('/', userLogin);
-router.post('/', userRegister);
-export default router;
-//# sourceMappingURL=user.js.map
