cd88f9d7657f42baae6bac8dc3a7922b39cc4088
heryanto <71817100+heryanto87@users.noreply.github.com>
2023-08-04 09:44:05 +0700

    delete jwt

--- routes/jwt.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { jwtVerify } from "../controllers/jwt.js";
-
-const router = express.Router();
-
-router.post("/verify", jwtVerify);
-
-export default router;
